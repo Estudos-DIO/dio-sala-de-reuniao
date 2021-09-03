6661379cf3e966bade7a5c3c9df466fab174f251
Mauro Salomão <oruamsalomao@gmail.com>
2021-09-02 22:33:37 -0300

    Arquivos base.

---
+++ src/main/java/com/sala_de_reuniao/SalaDeReuniaoApplication.java
@@ -0,0 +1,13 @@
+package com.sala_de_reuniao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaDeReuniaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaDeReuniaoApplication.class, args);
+	}
+
+}
