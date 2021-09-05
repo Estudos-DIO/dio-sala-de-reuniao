58fb0bc43f462aaa70fb60bfd5ea0fe4e39a5b93
Mauro Salomão <oruamsalomao@gmail.com>
2021-09-05 10:59:04 -0300

    Mensagem default para exceções.

--- src/main/java/exception/DetalhesDoErro.java
+++ src/main/java/exception/DetalhesDoErro.java
@@ -1,20 +1,36 @@
 package exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class DetalhesDoErro {
 
     private Date timestamp;
     private String mensagem;
     private String detalhes;
 
+    public DetalhesDoErro() {
+        super();
+    }
+
+    public DetalhesDoErro(Date timestamp, String mensagem, String detalhes) {
+        super();
+        this.timestamp = timestamp;
+        this.mensagem = mensagem;
+        this.detalhes = detalhes;
+    }
+
+    @Override
+    public String toString() {
+        return "DetalhesDoErro [" +
+                "timestamp='" + timestamp + "'" +
+                ", mensagem='" + mensagem + "'" +
+                ", detalhes='" + detalhes + " ]";
+    }
 }
