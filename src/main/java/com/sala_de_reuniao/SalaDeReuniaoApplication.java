d0b55bc0616f2a23128c57044928f9660ba19abb
Mauro Salomão <oruamsalomao@gmail.com>
2021-09-05 11:53:23 -0300

    Inclusão das classes de exceção.

--- src/main/java/com/sala_de_reuniao/SalaDeReuniaoApplication.java
+++ src/main/java/com/sala_de_reuniao/SalaDeReuniaoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EntityScan(basePackages = { "model" })
 @EnableJpaRepositories(basePackages = { "repository" })
-@ComponentScan(basePackages = {"controller;service;"})
+@ComponentScan(basePackages = {"controller;service;exception;"})
 public class SalaDeReuniaoApplication {
 	public static void main(String[] args) {
 	SpringApplication.run(SalaDeReuniaoApplication.class, args);
